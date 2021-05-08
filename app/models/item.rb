20543233370b5cb3358d9c8fd4943931d483eca9
HamazakiHisanori <hiii1023@icloud.com>
2021-05-08 16:06:56 +0900

    商品出品機能実装5

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,18 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :delivery_charge_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :day_id, numericality: { other_than: 1 }
     validates :price,
               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'is invalid' }
-    validates :user
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_charge_id
+      validates :prefecture_id
+      validates :day_id
+    end
   end
+
   belongs_to :user
   has_one_attached :image
 
